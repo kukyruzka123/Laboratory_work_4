daad75990f179e9754eb6936ea60e07946467001
kukyruzka@outlook.com <kukyruzka@outlook.com>
2020-06-23 11:44:57 +0400

    Добавьте файлы проекта.

---
+++ Лаба_4/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Лаба_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.inputPercentNumber = new System.Windows.Forms.TextBox();
+            this.inputNumber = new System.Windows.Forms.TextBox();
+            this.reset = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.outputNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(63, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(498, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Калькулятор процентов – нахождение процента от числа";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(16, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Сколько составляет";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(273, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "% от числа";
+            // 
+            // inputPercentNumber
+            // 
+            this.inputPercentNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputPercentNumber.Location = new System.Drawing.Point(188, 64);
+            this.inputPercentNumber.Name = "inputPercentNumber";
+            this.inputPercentNumber.Size = new System.Drawing.Size(64, 26);
+            this.inputPercentNumber.TabIndex = 3;
+            // 
+            // inputNumber
+            // 
+            this.inputNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inputNumber.Location = new System.Drawing.Point(373, 64);
+            this.inputNumber.Name = "inputNumber";
+            this.inputNumber.Size = new System.Drawing.Size(70, 26);
+            this.inputNumber.TabIndex = 4;
+            // 
+            // reset
+            // 
+            this.reset.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.reset.Location = new System.Drawing.Point(468, 64);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(78, 26);
+            this.reset.TabIndex = 5;
+            this.reset.Text = "Сброс";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // next
+            // 
+            this.next.BackColor = System.Drawing.Color.ForestGreen;
+            this.next.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.next.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.next.Location = new System.Drawing.Point(256, 191);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(111, 37);
+            this.next.TabIndex = 6;
+            this.next.Text = "Расчитать";
+            this.next.UseVisualStyleBackColor = false;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // outputNumber
+            // 
+            this.outputNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outputNumber.Location = new System.Drawing.Point(231, 126);
+            this.outputNumber.Multiline = true;
+            this.outputNumber.Name = "outputNumber";
+            this.outputNumber.Size = new System.Drawing.Size(152, 35);
+            this.outputNumber.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 254);
+            this.Controls.Add(this.outputNumber);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.inputNumber);
+            this.Controls.Add(this.inputPercentNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox inputPercentNumber;
+        private System.Windows.Forms.TextBox inputNumber;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.TextBox outputNumber;
+    }
+}
+
---
+++ Лаба_4/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаба_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            double num = Convert.ToDouble(inputNumber.Text);
+            double Percent = Convert.ToDouble(inputPercentNumber.Text);
+
+            outputNumber.Text = Convert.ToString(num / 100 * Percent);
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            inputNumber.Clear();
+            inputPercentNumber.Clear();
+            outputNumber.Clear();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyValue == (char)Keys.Enter)
+            {
+                next_Click(next, null);
+            }
+        }
+    }
+}
